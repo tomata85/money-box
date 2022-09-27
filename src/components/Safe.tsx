2a3f20ecc3c07db7be43aa736ad4d56b04c75c63
Talya <talya@Talya.local>
2022-09-27 12:42:41 +0300

    add safe properties to view

--- src/components/Safe.tsx
+++ src/components/Safe.tsx
@@ -1,26 +1,35 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
 import { ISafe } from '../types';
 
 function Safe(props: { safe: ISafe }) {
   const { safe }= props
+  const savingsAmount = `יש לי 15 שקלים בקופה`
+  const goalText = `המטרה הבאה שלי: לרכוש ${safe.goalName} במחיר ${safe.goalAmount} שקלים`
+  const lastDeposit = `הפקדה אחרונה בוצעה לפני 6 ימים`
+  const lastWithrawal = `משיכה אחרונה בוצעה לפני 10 ימים`
+  const daysUntilBonus = `עוד 10 ימים עד לבונוס הבא!`
+
   return (
     <Card
        bg={'info'}
        key={'info'}
-       style={{ width: '40rem' }}
+       style={{ width: '30rem' }}
        className="mb-2"
      >
       <Card.Img variant="top" src={safe.photoUrl} />
       <Card.Body>
         <Card.Title>{safe.name}</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Card.Text>{savingsAmount}</Card.Text>
       </Card.Body>
+      <ListGroup className="list-group-flush">
+       <ListGroup.Item>{goalText}</ListGroup.Item>
+       <ListGroup.Item>{lastDeposit}</ListGroup.Item>
+       <ListGroup.Item>{lastWithrawal}</ListGroup.Item>
+       <ListGroup.Item>{daysUntilBonus}</ListGroup.Item>
+     </ListGroup>
     </Card>
   );
 }
