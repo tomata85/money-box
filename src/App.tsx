3975ac947109c0db2979066c5754f7013beb413c
Talya <talya@Talya.local>
2022-09-27 22:14:28 +0300

    add flex

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { ISafe } from './types'
 import Main from './components/Main';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import './App.css'
 
 function App() {
   const [safes, setSafes] = useState<ISafe[]>([]);
@@ -15,10 +19,20 @@ function App() {
          });
    }, []);
 
+const title = 'הבנק של משפחת גונן'
+
 return (
-    <div className="App">
-      <Main safes={safes} />
-    </div>
+    <>
+      <Navbar className="header" bg="dark" variant="dark" sticky="top">
+          <Container>
+            <Navbar.Brand>🐽 Money Box</Navbar.Brand>
+          </Container>
+      </Navbar>
+      <div className='app-body'>
+        <h1 className='app-title'>{title}</h1>
+        <Main safes={safes} />
+      </div>
+    </>
   );
 }
 
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,19 +2,11 @@ import React from 'react'
 import { ISafe } from '../types'
 import Safe from './Safe'
 import CardGroup from 'react-bootstrap/CardGroup';
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
 
 function Main(props: {safes: ISafe[]}) {
   return (
     <>
-      <Navbar className="header" bg="dark" variant="dark" sticky="top">
-          <Container>
-            <Navbar.Brand>🐽 Money Box</Navbar.Brand>
-          </Container>
-      </Navbar>
-      <CardGroup>
+      <CardGroup className="card-list">
         {props.safes.map(safe => {
             const props = {safe: safe };
             return (<Safe key={safe.id} {...props} />)}
