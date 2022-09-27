8c0dcb7deec2d8b457a92454e0faad4785f20d75
Talya <talya@Talya.local>
2022-09-27 15:36:11 +0300

    ltr header

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,15 +2,25 @@ import React from 'react'
 import { ISafe } from '../types'
 import Safe from './Safe'
 import CardGroup from 'react-bootstrap/CardGroup';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 function Main(props: {safes: ISafe[]}) {
   return (
-    <CardGroup>
-      {props.safes.map(safe => {
-          const props = {safe: safe };
-          return (<Safe key={safe.id} {...props} />)}
-      )}
-    </CardGroup>
+    <>
+      <Navbar className="header" bg="dark" variant="dark" sticky="top">
+          <Container>
+            <Navbar.Brand>🐽  Money Box</Navbar.Brand>
+          </Container>
+      </Navbar>
+      <CardGroup>
+        {props.safes.map(safe => {
+            const props = {safe: safe };
+            return (<Safe key={safe.id} {...props} />)}
+        )}
+      </CardGroup>
+    </>
   )
 }
 
