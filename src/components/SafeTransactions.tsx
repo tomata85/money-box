e59ecd76e397e97b1ae1c68d367dd36ca49cca28
Talya <talya@Talya.local>
2022-09-30 15:54:47 +0300

    added missing file

---
+++ src/components/SafeTransactions.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { ISafe } from '../types'
+import Table from 'react-bootstrap/Table';
+import { ToString } from '../utils'
+import ReactTimeAgo from 'react-time-ago'
+
+function SafeTransactions(props: {safe: ISafe}) {
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>תאריך</th>
+          <th>פעולה</th>
+          <th>סכום</th>
+          <th>סיבה</th>
+        </tr>
+        </thead>
+        <tbody>
+          {props.safe.transactions.map(tran => (
+            <tr>
+              <td>
+                <ReactTimeAgo date={new Date(tran.timestamp)} locale="he-il"/>
+              </td>
+              <td>{ToString(tran.type)}</td>
+              <td className="ltr-table-cell">{tran.amount}</td>
+              <td>{tran.reason}</td>
+            </tr>
+          ))}
+        </tbody>
+    </Table>
+  )
+}
+
+export default SafeTransactions;
