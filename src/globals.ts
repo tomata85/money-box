5792ae5cd91f0ef72217bc72afef35d52cc49feb
Talya <talya@Talya.local>
2022-10-12 16:22:46 +0300

    use prod db

--- src/globals.ts
+++ src/globals.ts
@@ -1,2 +1,2 @@
-// export const DB_URL = "https://money-box-db.herokuapp.com";
-export const DB_URL = "http://localhost:3000";
+export const DB_URL = "https://money-box-db.herokuapp.com";
+// export const DB_URL = "http://localhost:3000";
